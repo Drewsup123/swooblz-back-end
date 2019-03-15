cdcc551e02871f06ef60c6c3c563337e4f4c6f0b
Drew <drewsup123c@gmail.com>
2019-03-15 16:45:28 -0600

    added knexfile set up for deployment

---
+++ knexfile.js
@@ -0,0 +1,54 @@
+// Update with your config settings.
+
+const dbConnection = process.env.DATABASE_URL || localPgConnection;
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault : true,
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './database/seeds' },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: "./database/migrations",
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: 'db_connection',
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: "./database/migrations"
+    },
+    useNullAsDefault : true,
+    ssl : true,
+  }
+
+};
