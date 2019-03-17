57c0b7507cdbf55dce7ab4f4ec8d398ade321f4c
Drew <drewsup123c@gmail.com>
2019-03-16 19:45:30 -0600

    reconfigure db enviroment

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,5 +1,8 @@
-const knex = require("knex");
-const config = require("../knexfile");
-const dbEnv = process.env.DB_ENV || "development";
-console.log(dbEnv);
-module.exports = knex(config[dbEnv]);
+// const knex = require("knex");
+// const config = require("../knexfile");
+// const dbEnv = process.env.DB_ENV || "development";
+// console.log(dbEnv);
+// module.exports = knex(config[dbEnv]);
+const dbEnv = process.env.DB_ENV || 'development';
+const config = require('../knexfile.js')[dbEnv];
+module.exports = require('knex')(config);
